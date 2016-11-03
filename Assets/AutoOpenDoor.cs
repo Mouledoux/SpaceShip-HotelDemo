3af31bada24830ac2131c6f077862617d691aba3
Zac King <ZacKingx@gmail.com>
2016-11-03 17:20:38 -0500

    vive integration

---
+++ Assets/AutoOpenDoor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoOpenDoor : MonoBehaviour
+{
+    Animator anim;
+    //AudioSource audio;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        //audio = GetComponent<AudioSource>();
+    }
+
+    [ContextMenu("Open door")]
+    public void Test()
+    {
+        anim.SetBool("PlayerNear", true);
+        //audio.Play();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.SetBool("PlayerNear", true);
+            //audio.Play();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.SetBool("PlayerNear", false);
+            //audio.Play();
+        }
+    }
+}
